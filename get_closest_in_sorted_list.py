dffc9606fb5d78e575f69cc4198a7929849f9897
Nagy-Zoltan <47283309+Nagy-Zoltan@users.noreply.github.com>
2023-12-02 02:25:22 +0100

    Create get_closest_in_sorted_list.py

---
+++ get_closest_in_sorted_list.py
@@ -0,0 +1,18 @@
+from bisect import bisect_left
+from math import isclose
+
+
+def get_closest_in_sorted_list(L: list[float], num: float):
+    closest_candidates = {}
+    bisect_index = bisect_left(L, num)
+    for i in range(-1, 2):
+        try:
+            index = bisect_index + i
+            if index < 0:
+                continue
+            closest_candidates[index] = L[index]
+        except IndexError:
+            pass
+        
+    print(closest_candidates)
+    return min(closest_candidates, key=lambda key: abs(closest_candidates[key] - num))
